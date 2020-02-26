0db24198e8d5f78562279eab1c75fe6ea230fdbe
chula <codechula@gmail.com>
2020-02-26 16:19:34 +0300

    updated connection in esv

--- config/database.php
+++ config/database.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv('DATABASE_URL'));
+$host=$url["host"];
+$user=$url["user"];
+$password=$url["pass"];
+$database=substr($url["path"]);
+
 return [
 
     /*
