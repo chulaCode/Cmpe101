5728989769d9284a467d616490fe250e2bf0931a
chula <codechula@gmail.com>
2020-03-11 17:57:21 +0300

    s3 wahala

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -47,7 +47,7 @@
                           @endforeach
                           <div class="row">
                           <div class="col-12 d-flex justify-content-center mt-2">
-                             {{ $lecture1->links() }}
+                             
                             </div> 
                           <div class="d-lg-none col-12 d-flex justify-content-center mt-1"><a href="/game/{{$user->id}}">
                               <button class="btn btn-primary ">Play Binary Game</button>
