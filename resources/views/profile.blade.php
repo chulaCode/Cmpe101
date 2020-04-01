3d3e06d3896b10cbe171fd5cfc449ba28e028bff
chula <codechula@gmail.com>
2020-04-01 12:10:10 +0300

    fixing margin

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-        <div id="particles-js">
+      <div id="particles-js">
          <div class="container-fluid">
             <div class="row">
                   <div class="sidebar col-lg-3 col-md-12">
@@ -11,7 +11,7 @@
                         <div id="profileimage"><img src="{{ $user->profileImage() }}" class="rounded-circle w-50 pb-2 mx-lg-5 mx-md-4" alt=""/></div>
                         <div class=" h4 mx-lg-5 px-lg-5 mx-md-4" id="username">  {{$user->username}}  </div>
 
-                        <div id="edit"> <a href="#"><button class="btn btn-primary mb-3 mx-lg-5 px-lg-4 mx-md-4">Static Profile Image</button></a> </div>
+                        <div id="edit"> <a href="#"><button class="btn btn-primary mb-3 mx-lg-5 px-lg-4 mx-md-4">Static Profile</button></a> </div>
                         
                         <hr>
                         <p> Click on the play binary game to play game so you can earn points and badges Tips button is a guide to how</p>
@@ -36,47 +36,45 @@
                     <!-- <h5 ml-4><a href="/profile/create">Add New Post</a></h5>-->
                   </div>
                </div>
-               <div>
-                    <div class="notes my-3"> 
+                    <div class="notes"> 
 
                         <div class="instruction" style="margin-right:13rem">
-                           <h2 class="text-white inst" style="margin-left:16rem">INSTRUCTIONS</h2>
-                           <hr class="bg-white ">
-                           <div class="mt-4 text-white">
-                              <h3 class=""> 
-                              1) Check out lecture note on binary numbering system from course moodle <a href="https://moodle.ciu.edu.tr/course/view.php?id=17" target="_blank">Cmpe 101 notes</a>
-                              </h3 >
-                              <h3 class="my-3">
-                              2) Focus on just binary to decimal conversion because the survey and questions is just on binary to decimal conversion
-                              </h3>
-                              <h3>
-                              3) You have 40 seconds for each game question
-                              </h3>
-                              <h3 class="my-3">
-                              4) After studying lecture notes you can proceed to play game
-                              </h3>
-                              <h3 class="my-3">
-                              5) Checkout game rules on game page before starting game GOOD LUCK!
-                              </h3>
-                             
-                           </div>
+                              <h2 class="text-white inst" style="margin-left:16rem">INSTRUCTIONS</h2>
+                              <hr class="bg-white ">
+                              <div class="mt-4 text-white">
+                                 <h3 class=""> 
+                                 1) Check out lecture note on binary numbering system from course moodle <a href="https://moodle.ciu.edu.tr/course/view.php?id=17" target="_blank">Cmpe 101 notes</a>
+                                 </h3 >
+                                 <h3 class="my-3">
+                                 2) Focus on just binary to decimal conversion because the survey and questions is just on binary to decimal conversion
+                                 </h3>
+                                 <h3>
+                                 3) You have 40 seconds for each game question
+                                 </h3>
+                                 <h3 class="my-3">
+                                 4) After studying lecture notes you can proceed to play game
+                                 </h3>
+                                 <h3 class="my-3">
+                                 5) Checkout game rules on game page before starting game GOOD LUCK!
+                                 </h3>
+                              
+                              </div>
+                           
                               <div class="d-lg-none col-12 d-flex justify-content-center mt-1">
                                  <a href="/game/{{$user->id}}">
                                     <button class="btn btn-primary ">Play Binary Game</button>
                                  </a>
                               </div>
                         
-                                    
-                              </div>
                         </div>
                  </div>
          
           <!-- end of col-9   -->
-      </div>
+         </div>
       
       <!-- end of row -->
      
-        </div>
+   </div>
     </section>
 
     <section>
