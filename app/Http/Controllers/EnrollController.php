74ce3588e9cb5571001b7853d70e25d0bcf2b008
chula <codechula@gmail.com>
2020-04-14 04:51:43 +0300

    yeah

--- app/Http/Controllers/EnrollController.php
+++ app/Http/Controllers/EnrollController.php
@@ -15,7 +15,8 @@ class EnrollController extends Controller
         if($request->submit == "course")
         {
         
-           return redirect('/home/1');
+           $id=1;
+           return view('home',compact('id'));
          // return redirect("/login")->with('warning', 'you need to login to enrol.');
           //echo "<script>alert(' you must login first!!')</script>";   
 
