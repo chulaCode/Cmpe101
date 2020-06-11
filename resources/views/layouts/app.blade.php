a4d7343074f180f53d1c7952ff5ee79af4f3520b
chula <codechula@gmail.com>
2020-06-11 13:58:42 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'CMPE 101') }}</title>
+    <title>Cmpe101</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
