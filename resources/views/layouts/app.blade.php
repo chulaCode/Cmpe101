1a56dce5ccf91d4f96e88e9bcd5470b83377df1b
chula <codechula@gmail.com>
2020-06-13 12:16:55 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,8 @@
             <div class="container">
            
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'CMPE 101') }}
+                     CMPE 101
+                    <!--{{ config('app.name', 'CMPE 101') }}-->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
